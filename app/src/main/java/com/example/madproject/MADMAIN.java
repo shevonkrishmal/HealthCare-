4612f3adab7be9bcf9c7efd5025b507c5b5e8e6a
kasuni <it19122038@my.sliit.lk>
2020-08-18 22:37:26 +0530

    ALL THE FILES

---
+++ app/src/main/java/com/example/madproject/MADMAIN.java
@@ -0,0 +1,14 @@
+package com.example.madproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MADMAIN extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_m_a_d_m_a_i_n);
+    }
+}
---
+++ app/src/main/java/com/example/madproject/mad4.java
@@ -0,0 +1,14 @@
+package com.example.madproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mad4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mad4);
+    }
+}
