4c7a6e64331d74adc06433a05a6be3c4f88cb178
shenaljfx <shenalsamaranayakejfx@gmail.com>
2020-08-23 18:53:13 +0530

    IT19033938 Samaranayake S.L

---
+++ app/src/main/java/com/example/healthcarepluse/advertising.java
@@ -0,0 +1,14 @@
+package com.example.healthcarepluse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class advertising extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advertising);
+    }
+}
