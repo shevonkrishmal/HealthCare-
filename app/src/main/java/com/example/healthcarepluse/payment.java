a2fbe2f1dfdf3e4773fb43540ba2af4b8c74e819
shenaljfx <shenalsamaranayakejfx@gmail.com>
2020-09-09 20:40:09 +0530

    IT19033938 Samaranayake S.L

--- app/src/main/java/com/example/healthcarepluse/payment.java
+++ app/src/main/java/com/example/healthcarepluse/payment.java
@@ -2,13 +2,28 @@ package com.example.healthcarepluse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class payment extends AppCompatActivity {
-
+    private Button BtnMove;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+
+        BtnMove = findViewById(R.id.paybtn);
+        BtnMove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                moveToMainActivity();
+            }
+        });
     }
+    private void  moveToMainActivity(){
+        Intent intent = new Intent(payment.this, MainActivity.class);
+        startActivity(intent);
+     }
 }
